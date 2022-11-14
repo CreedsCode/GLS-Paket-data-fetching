947a171d88c3e0d8322b2b9bcce8eb26d95919be
Dercio <contact@creedscode.cc>
2022-11-14 20:12:20 +0100

    Reverting to edcb991

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { BaseSyntheticEvent, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Papa from "papaparse";
 import { Navigate, useNavigate } from "react-router-dom";
 
@@ -6,55 +6,56 @@ import { Navigate, useNavigate } from "react-router-dom";
 const allowedExtensions = ["csv"];
 
 export const Home = () => {
-    const [columns, setFileColumns] = useState([]);
+    // This state will store the parsed data
+    const [data, setData] = useState([]);
+
+    // It state will contain the error when
+    // correct file extension is not used
     const [error, setError] = useState("");
+
+    // It will store the file uploaded by the user
     const [file, setFile] = useState("");
 
+    // This function will be called when
+    // the file input changes
     const handleFileChange = (e) => {
         setError("");
 
+        // Check if user has entered the file
         if (e.target.files.length) {
             const inputFile = e.target.files[0];
 
+            // Check the file extensions, if it not
+            // included in the allowed extensions
+            // we show the error
             const fileExtension = inputFile?.type.split("/")[1];
             if (!allowedExtensions.includes(fileExtension)) {
                 setError("Please input a csv file");
                 return;
             }
 
+            // If input type is correct set the state
             setFile(inputFile);
         }
     };
     const handleParse = () => {
+        // If user clicks the parse button without
+        // a file we show a error
         if (!file) return setError("Enter a valid file");
 
+        // Initialize a reader which allows user
+        // to read any file or blob.
         const reader = new FileReader();
 
+        // Event listener on reader when the file
+        // loads, we parse it and set the data.
         reader.onload = async ({ target }) => {
             const csv = Papa.parse(target?.result, { header: true });
             const parsedData = csv?.data;
             const columns = Object.keys(parsedData[0]);
-            console.log("parsing:", parsedData);
-
-            setFileColumns(columns);
+            setData(columns);
         };
         reader.readAsText(file);
-    };
-
-    const headerSelection = (e: BaseSyntheticEvent) => {
-        console.log(e);
-    };
-
-    const handleFetching = () => {
-        // Papa.parse(file, {
-        //     worker: true,
-        //     step: function (row) {
-        //         console.log("Row:", row.data);
-        //     },
-        //     complete: function () {
-        //         console.log("All done!");
-        //     },
-        // });
     };
 
     return (
@@ -69,35 +70,12 @@ export const Home = () => {
                 type="File"
             />
             <div>
-                <button className="btn" onClick={handleParse}>
-                    Read File
-                </button>
+                <button onClick={handleParse}>Parse</button>
             </div>
             <div style={{ marginTop: "3rem" }}>
-                {error ? (
-                    error
-                ) : (
-                    <>
-                        <label htmlFor="tracking_header">
-                            Choose the column with the Tracking Numbers
-                        </label>
-                        <select
-                            onChange={headerSelection}
-                            name="tracking_header"
-                            id="tracking_header"
-                        >
-                            <option value="" disabled selected hidden>
-                                Choose Header...
-                            </option>
-                            {columns.map((col, idx) => (
-                                <option value={idx} key={idx}>
-                                    {col}
-                                </option>
-                            ))}
-                        </select>
-                        <button onClick={handleFetching}>Bundle numbers</button>
-                    </>
-                )}
+                {error
+                    ? error
+                    : data.map((col, idx) => <div key={idx}>{col}</div>)}
             </div>
         </div>
     );
